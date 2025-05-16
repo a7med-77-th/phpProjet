1ab0e247cf6cd1d0803535bdbad8903cf19404ce
a7med-77-th <ahmedanouar2004@gmail.com>
2025-05-16 19:17:11 +0100

    Update Database.php

--- models/Database.php
+++ models/Database.php
@@ -1,2 +1,13 @@
 <?php
 // PDO connection
+$host = 'localhost';
+$db = 'nom_de_ta_base';
+$user = 'root';
+$pass = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
